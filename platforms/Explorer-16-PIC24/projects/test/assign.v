132ae2c070967c70219d2da61617688596f37dbf
fermarsan <fmartinezsanta@gmail.com>
2025-11-11 12:43:24 -0500

    platforms update

---
+++ platforms/Explorer-16-PIC24/projects/test/assign.v
@@ -0,0 +1,8 @@
+//declare and assign 
+fn main() {
+    
+    contador = 200
+    contador = 10
+    contador = - 4
+    contador = 5
+}
---
+++ platforms/Explorer-16-PIC24/projects/test/program_2.c
@@ -0,0 +1,14 @@
+//Generated C file for:  Device = PIC24FJ128GA010  Board = Explorer 16
+
+#include "settings.h"
+
+int main(void) {
+//AD1PCFG = 0xFFFF; // set all I/O digital
+
+	const int16_t a = 35;
+	const int16_t b = 2;
+	const float c = 4.5;
+	
+	
+	return 0;
+}
